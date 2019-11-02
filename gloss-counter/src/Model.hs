7f577cf075da4f31f1426b24908f6c659a49b75e
kasperdeneuzige <kasper_peeters@live.nl>
2019-11-02 15:07:12 +0100

    Klein beetje collision

--- gloss-counter/src/Model.hs
+++ gloss-counter/src/Model.hs
@@ -7,11 +7,12 @@ data Gamestate = Gamestate {objects :: Objects, settings :: Settings, elapsedTim
 data Objects = Objects {player :: Player, enemies :: Enemies}
 data Settings = Settings {}
 data Player = Player {spaceShip :: SpaceShip}
-data Enemies = Enemies {meteorites :: [Meteorite], spaceShips :: [SpaceShip], bullets :: [Bullet]}
+data Enemies = Enemies {meteorites :: [Meteorite], spaceShips :: [EnemySpaceShip], bullets :: [Bullet]}
 data Meteorite = Meteorite {shapeM :: Shape, healthM :: Float, speedM :: Point}
 data SpaceShip = SpaceShip {shapeSP :: Shape, healthSP :: Float, speedSP :: Point, fireRate :: Float, lastFire :: Float}
+data EnemySpaceShip = EnemySpaceShip {enemySpaceShip :: EnemySpaceShip}
 data Bullet = Bullet {shapeB :: Shape, healthB :: Float, speedB :: Point}
-data Shape = Rectangle Point Point Point Point
+data Shape = Rectangle Point Point Point Point        
            | Circle Point Float
            | Triangle Point Point Point
 data Point = Point Float Float
